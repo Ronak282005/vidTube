ddbdcee1fda5db1c187ecd1a9f834b01371da19c
ronak <ronakpraja340@gmail.com>
2025-06-30 23:42:50 +0530

    db init in index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import {app} from "./app.js"
+import connectDb from "./db/index.js"
 import {config} from "./utils/config.js"
 
 const PORT = config.PORT || 8080
 
-app.listen(PORT,()=>{
-    console.log(`listening on the port ${PORT}`);
-})
+connectDb()
+    .then(()=> {
+        app.listen(PORT,()=>{
+            console.log(`listening on the port ${PORT}`);
+        })
+    })
+    .catch(()=>{
+        console.log("failed to connect db");
+    })
