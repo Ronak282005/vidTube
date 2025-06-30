1fbc34fbfcc48f226b40c8f7d350eb08352dca6c
ronak <ronakpraja340@gmail.com>
2025-06-30 23:50:28 +0530

    debugs

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { config } from "../utils/config.js";
 
 const connectDb = async () => {
     try {
-        const connection = await connect(configDotenv.DB_URL)
+        const connection = await connect(config.DB_URL)
         console.log("connected to db",connection.connection.host);
     } catch (error) {
         console.log(error);
