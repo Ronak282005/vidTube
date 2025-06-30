f6eca5aa06617252ad4b0e9c3318fa94c8526ba6
ronak <ronakpraja340@gmail.com>
2025-06-30 18:26:00 +0530

    dotenv config

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
-  port: process.env.PORT,
-  dbUrl: process.env.DB_URL,
-  jwtSecret: process.env.JWT_SECRET,
+  PORT: process.env.PORT,
+  DB_URL: process.env.DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
