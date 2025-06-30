ea1e061c59d5690239c682de0a5ce452ec024b91
ronak <ronakpraja340@gmail.com>
2025-06-30 23:40:55 +0530

    db init

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,13 @@
 import { connect } from "mongoose";
 import { config } from "../utils/config";
 
-connect(config.DB_URL, () => {
-  console.log("connected to");
-});
+const connectDb = async () => {
+    try {
+        const connection = await connect(config.DB_URL)
+        console.log("connected to db");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default connectDb
